40aaed019523def874bc0c0a76457ac65cd322b3
Mykolas Kaminskas <mykolaskam@gmail.com>
2019-01-25 04:46:48 +0000

    connection to database for sales orders, basic routing and twig display

---
+++ includes/wms_app/app/routes/newsalesorder.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mykolaskaminskas
+ * Date: 2019-01-25
+ * Time: 04:14
+ */
---
+++ includes/wms_app/app/src/SalesOrder.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mykolaskaminskas
+ * Date: 2019-01-25
+ * Time: 01:49
+ */
+
+class SalesOrder
+{
+
+}
